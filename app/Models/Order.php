75b1c9c1961e0ce2b0a2e3d853956633a7e2ddfc
Danial Barazandeh <Gabriel.barazandeh@gmail.com>
2023-01-15 20:30:14 +0330

    order
    order  is added

---
+++ app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    protected $table = 'order';
+    protected $fillable = [
+        'user_id',
+        'vendor_id',
+        'product_id',
+        'price_at_time',
+        'sale_price_at_time',
+        'status'
+    ];
+
+
+    public function vendor()
+    {
+        return $this->hasOne(Vendor::class,'id','vendor_id');
+    }
+    public function user()
+    {
+        return $this->hasOne(Vendor::class,'id','user_id');
+    }
+    public function product()
+    {
+        return $this->hasOne(Vendor::class,'id','product_id');
+    }
+}
